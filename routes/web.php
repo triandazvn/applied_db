1f690e7fe2e53e3c7c6a76fc5117bd0afbd33cda
bangkitbali <s160422163@student.ubaya.ac.id>
2025-06-10 15:19:09 +0700

    coba login

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.dashboard');
+
 });
+//  -> middleware('auth')
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
